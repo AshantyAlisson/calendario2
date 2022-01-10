6e4840f79745c300ffb8e24b058c888c10b7e815
AshantyAlisson <alisson.ashanty.100@gmail.com>
2022-01-09 21:16:36 -0600

    Aqui estan los formularios

--- src/app/formulario-visita/formulario-visita.module.ts
+++ src/app/formulario-visita/formulario-visita.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormularioVisitaComponent } from './formulario-visita/formulario-visita.component';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { DatosPersonalesComponent } from './datos-personales/datos-personales.component';
+import { DisponibilidadComponent } from './disponibilidad/disponibilidad.component';
+import { ReferenciaComponent } from './referencia/referencia.component';
 
 
 
 @NgModule({
   declarations: [
-    FormularioVisitaComponent
+    FormularioVisitaComponent,
+    EncabezadoComponent,
+    DatosPersonalesComponent,
+    DisponibilidadComponent,
+    ReferenciaComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    FormularioVisitaComponent
+    FormularioVisitaComponent,
+    EncabezadoComponent,
+    DatosPersonalesComponent,
+    DisponibilidadComponent,
+    ReferenciaComponent
   ]
 })
 export class FormularioVisitaModule { }
